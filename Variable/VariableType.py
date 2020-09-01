d3e5f6ea8214cfd24b1ed2c624f2f0be2e0d3684
zichen <18515489929@163.com>
2020-09-02 00:35:00 +0800

    variable

---
+++ Variable/VariableType.py
@@ -0,0 +1,14 @@
+# -*- coding utf-8 -*-
+# @Time: 2020/9/2 0:26
+# @Author:
+# @Software:PyCharm
+
+#赋值整型变量
+counter = 100
+#赋值浮点型
+miles = 1000.0
+#字符串
+name = "John"
+print(counter)
+print(miles)
+print(name);
