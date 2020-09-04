d83166c5c981112facd8294d1b48fdffe770fe25
子辰 <18515489929@163.com>
2020-09-05 01:40:17 +0800

    基本数据类型的总结

---
+++ Variable/Base/SummarizeBaseDataType.py
@@ -0,0 +1,18 @@
+# -*- coding utf-8 -*-
+# @ProjectName: py_zichen
+# @Script: SummarizeBaseDataType
+# @Discription: 基本数据类型的总结
+# @Author: 子辰
+# @Date: 2020-09-05 01:35
+# @Software: PyCharm
+
+"""
+Python基本数据类型：
+数字(Number)              序列(sequence)            字典(dict)            集合(collection)
+    int(整型)                 list(列表)                无序                  set(无序，不可重复，没有索引)
+    float(浮点型)                  可变序列              没有索引
+    complex(复数)             str(字符串)               通过键访问值
+    bool(布尔)                    不可变序列             键不可改变
+                              tuple(元组)
+                                  不可变序列
+"""
