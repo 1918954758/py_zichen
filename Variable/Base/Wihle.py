3dd9ea1b41534cb60ee130321000e1d52b6b821e
子辰 <18515489929@163.com>
2020-09-05 19:45:56 +0800

    while 循环

---
+++ Variable/Base/Wihle.py
@@ -0,0 +1,22 @@
+# -*- coding utf-8 -*-
+# @ProjectName: py_zichen
+# @Script: Wihle
+# @Discription:  while 循环
+# @Author: 子辰
+# @Date: 2020-09-05 19:37
+# @Software: PyCharm
+
+"""
+for循环和while循环的区别：
+for     通过循环遍历一个可迭代对象来构建循环，可迭代对象遍历完成后，停止循环
+while   只有当条件为假的时候，才会停止循环
+"""
+'''
+基本形式：
+while <条件>:
+    <语句1>
+else:
+    <语句2>
+
+对于while循环的使用，循环体内必须得存在更改条件的语句，否则很容易进入死循环
+'''
