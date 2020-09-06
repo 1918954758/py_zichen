a642e7f4f56ce2701650c88f28e2da87886d57c0
子辰 <18515489929@163.com>
2020-09-06 19:41:59 +0800

    增加group()组的匹配

--- Variable/RegexExpression/GroupMatch.py
+++ Variable/RegexExpression/GroupMatch.py
@@ -33,3 +33,33 @@ list2 = re.findall('(lemon){3}', match_str)
 print(list2)                # output------->['lemon']
 list3 = re.findall('[lemon]{3}', match_str)
 print(list3)                # output------->['lem', 'onl', 'emo', 'nle', 'mon']
+
+"""
+Group组的匹配
+"""
+print('--------------group()分组函数--------------')
+match_str1 = 'life is mostly happy,but sometimes sad'
+'''
+需求：
+    1. 将is mostly happy,but sometimes匹配出来
+    2. 将is mostly happy 以及sometimes分别匹配出来
+'''
+# 需求 1
+r = re.search('life(.*)sad', match_str1)
+print(r.group(0))               # output------>life is mostly happy,but sometimes sad
+print(r.group(1))               # output------>is mostly happy,but sometimes
+# print(r.group(2))  该语句要想顺利执行需要将 sad 放到一个组里才可以，，不然python找不到该组
+
+# 需求 2
+r1 = re.search('life(.*)but(.*)sad', match_str1)
+print(r1.group(1))              # output------>is mostly happy,
+print(r1.group(2))              # output------>sometimes
+
+# 如果想查看所有分组的情况，我们可以使用groups()函数
+print(r1.groups())              # output------>(' is mostly happy,', ' sometimes ')
+'''
+补充一下group()函数：
+    该函数默认情况是等价于传参数0的，也就是把所有的匹配结果都返回
+    如果参数传递的是1，那么返回第一个分组情况
+    如果参数传递的是2，那么返回第二个分组情况
+'''
