908659fe2bc65de322ca874b587ad5d3452d1c2a
子辰 <18515489929@163.com>
2020-09-08 21:43:46 +0800

    调整编码

--- Variable/FileAndException/WriteFile.py
+++ Variable/FileAndException/WriteFile.py
@@ -35,16 +35,16 @@ data1.close()
 '''
 2. 写文件
 '''
-with open('D:\py_zichen\Variable\FileAndException\测试写文件1.txt', mode = 'w', encoding = 'utf-8') as data2:
+with open('D:\py_zichen\Variable\FileAndException\测试写文件1.txt', mode = 'a', encoding = 'utf-8') as data2:
     data2.write('又写入了一个lemon\n又写入了一个lemon')
 '''
 3. 写文件
 '''
-with open('D:\py_zichen\Variable\FileAndException\测试写文件1.txt', mode = 'w', encoding = 'utf-8') as data3:
+with open('D:\py_zichen\Variable\FileAndException\测试写文件1.txt', mode = 'a', encoding = 'utf-8') as data3:
     data3.writelines('lemonlemonlemonlemonlemonlemonlemon\nlemonlemonlemone')
 '''
 4. 写文件，传入序列
 '''
-with open('D:\py_zichen\Variable\FileAndException\测试写文件1.txt', mode = 'a', encoding = 'iso8859-1') as data4:
+with open('D:\py_zichen\Variable\FileAndException\测试写文件1.txt', mode = 'a', encoding = 'utf-8') as data4:
     data4.writelines(['lemon\n', 'lemon\n', 'lemon\n', 'lemon\n', 'lemon\n'])
 
