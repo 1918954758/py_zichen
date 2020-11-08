6e1a09e1af8e9bfb8d4d157cb688ca916c9e6e39
e <18515489929.@163.com>
2020-11-08 16:13:25 +0800

    python数字(Number)

---
+++ python_base/PythonCode/python基本数据类型/数学函数.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python
+# -*- coding: UTF-8 -*-
+
+import math
+
+if __name__ == '__main__':
+    print(abs(-2))
+    print(math.ceil(2.3))
+    print(math.exp(1))
+    print(math.fabs(-10))
+    print(math.floor(4.9))
+    print(math.log(math.e))
+    print(math.log10(100))
+    print(max(1, 2, 3, 2, 5, 6))
+    print(min(3, 5, 1, 7, 3, 9, 0))
+    print(math.modf(3.64))
+    print(math.pow(2, 5))
+    print(round(3.6))
+    print(round(3.2))
+    print(math.sqrt(25))
+    print(int(math.sqrt(25)))
