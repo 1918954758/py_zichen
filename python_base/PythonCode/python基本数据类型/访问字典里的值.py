eaa8738c58840b3eeb5b7a401d1133e1ded92754
e <18515489929.@163.com>
2020-11-10 19:43:00 +0800

    python字典

---
+++ python_base/PythonCode/python基本数据类型/访问字典里的值.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python
+# -*- coding: UTF-8 -*-
+
+dict = {'Name': 'Runoob', 'Age': 21, 'Class': 'First'}
+
+if __name__ == '__main__':
+    print("dict['Name']：", dict['Name'])
+    print("dict['Age']：", dict["Age"])
