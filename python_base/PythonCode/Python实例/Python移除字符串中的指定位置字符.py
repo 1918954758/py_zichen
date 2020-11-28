6584b4d42ccafa4dcd29b368e40bb3ecade62d2f
e <18515489929.@163.com>
2020-11-28 23:03:15 +0800

    Python实例

--- python_base/PythonCode/Python实例/Python移除字符串中的指定位置字符.py
+++ python_base/PythonCode/Python实例/Python移除字符串中的指定位置字符.py
@@ -2,11 +2,12 @@
 # -*- coding: UTF-8 -*-
 def remove1(s, n):
     li = list(s)
-    del li[n-1]
+    del li[n - 1]
     st = ''
     for i in range(0, len(li)):
         st += li[i]
     return st
+
 
 if __name__ == '__main__':
     print(remove1('ziichen', 3))
